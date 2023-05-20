93f03b48be8a8cafa1bd3550fed1d663b00ee170
idamanukyan <84736965+idamanukyan@users.noreply.github.com>
2023-05-20 22:58:31 +0400

    Add files via upload

---
+++ bank-account-management/account-entities/src/main/java/com/iunetworks/dtos/legalEntity/LegalEntityAccountTransferDto.java
@@ -0,0 +1,14 @@
+package com.iunetworks.dtos.legalEntity;
+
+public class LegalEntityAccountTransferDto {
+
+  private String accountNumber;
+
+  public String getAccountNumber() {
+    return accountNumber;
+  }
+
+  public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+  }
+}
